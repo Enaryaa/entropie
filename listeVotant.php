f38b8cf7b128c366fd82f4b51108a41f80db6517
mikembo <brice.mikembo@etu.u-pec.fr>
2020-03-28 22:25:23 +0100

    Correction listeVotant

--- listeVotant.php
+++ listeVotant.php
@@ -18,51 +18,30 @@
 
 
 <?php
-$json = 'http://www.iut-fbleau.fr/projet/maths/?f=logins.json';
-$json2= 'http://www.iut-fbleau.fr/projet/maths/?f=pagerank.json';
 
-$jsondata = file_get_contents($json);
-$jsondata2= file_get_contents($json2);
+function liste(){
+    $json = 'http://www.iut-fbleau.fr/projet/maths/?f=logins.json';
+    $json2= 'http://www.iut-fbleau.fr/projet/maths/?f=pagerank.json';
 
-$data = json_decode($jsondata,true);
-$vote = json_decode($jsondata2, true);
+    $jsondata = file_get_contents($json);
+    $jsondata2= file_get_contents($json2);
 
-//$result = array_merge($data, $vote);
+    $data = json_decode($jsondata,true);
+    $vote = json_decode($jsondata2, true);
 
-asort($data);
-//asort($vote);
+    asort($data);
 
-/*foreach($data as $cle=>$valeur) {
-    echo($cle.' ');
-}*/
+    $comp=array_intersect_key($data,$vote);
 
-/*foreach($vote as $cle=>$valeur) {
-    echo($cle.' ');
+    $result=array_merge($comp,$vote);
+
+    $tri = array_intersect_key($result, $comp);
+
+    $result2=array_merge($comp,$tri);
+
+    return $result2;
+
 }
-echo(count($vote));*/
-
-//var_dump($vote);
-
-//tableaux contenant les bons logins
-$comp=array_intersect_key($data,$vote);
-/*foreach ($comp as $key => $value) {
-    print_r($value.' ');
-}*/
-
-$result=array_merge($comp,$vote);
-/*foreach ($result as $key => $value) {
-    print_r($key.' ');
-}*/
-$tri = array_intersect_key($result, $comp);
-/*foreach ($tri as $key => $value) {
-    print_r($key.' ');
-}*/
-
-$result2=array_merge($comp,$tri);
-
-//tableaux avec votes des bon logins
-var_dump($result2);
-//var_dump($comp);
 
 ?>
 	<div id='myDiv'><!-- Plotly chart will be drawn inside this DIV --></div>
