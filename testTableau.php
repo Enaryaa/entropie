3d3d01e0711075a6108537d2653141561cfb1065
mikembo <brice.mikembo@etu.u-pec.fr>
2020-03-30 23:07:13 +0200

    calcul

--- testTableau.php
+++ testTableau.php
@@ -10,13 +10,17 @@ $json2= 'http://www.iut-fbleau.fr/projet/maths/?f=pagerank.json';
 
 $data = liste();
 
-
 $distributionLogin = distributionLogin($data);
 
 $cas_par_matiere = nbreCasPossible($distributionLogin);
 $distributionGlobal = distributionGlobal($data,$cas_par_matiere);
+
+$entropie = entropie($distributionLogin,$distributionGlobal);
+var_dump($entropie);
 //var_dump($cas_par_matiere);
-var_dump($distributionGlobal);
-//var_dump($distributionLogin);
+//echo $cas_par_matiere[1];
+//var_dump($distributionGlobal);
+//var_dump($distributionLogin['besnarda']);
+//echo $distributionGlobal['ACDA']['thor'];
 //echo $distributionLogin['bargoni']['ACDA']['mana'];
 ?>
